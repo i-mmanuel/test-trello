665a34ebfddd214c7e76b22ad7a4e1c7349f0068
Emmanuel Adatsi <emmanuel.jnr@outlook.com>
2019-05-17 22:13:52 +0100

    Initial commit

---
+++ assets/javascripts/application.js
@@ -0,0 +1,31 @@
+function dragStartHandler(event) {
+    console.log("Drag has started");
+    event.dataTransfer.setData("text/html", event.target.id);
+};
+
+function allowDrop(event) {
+    event.preventDefault();
+};
+
+function dropHandler(event) {
+    event.preventDefault();
+    var data = event.dataTransfer.getData("text/html");
+    event.target.append(document.getElementById(data));
+};
+
+$(function() {
+    $("div#rename-board form").on("submit", function(event) {
+        event.preventDefault();
+        var current_title = $("#menu-bar h1").html();
+    });
+
+    $("a#board-title").on("click", function(event) {
+        var current_title = $("#menu-bar h1").html();
+        $("#rename-board-input").attr("value", current_title)
+        $("div#rename-board").toggle();
+    });
+
+    $("img#rename-board-close-button").on("click", function() {
+        $("div#rename-board").toggle();
+    })
+})
