e36de5f408aebea778e345bf697f6e769e8f203c
Emmanuel Adatsi <emmanuel.jnr@outlook.com>
2019-05-25 13:26:17 +0100

    Added 'Click rename' functionality
    That functionality and some design elements added

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -12,6 +12,15 @@ function dropHandler(event) {
     event.target.closest("ul").append(document.getElementById(data));
 };
 
+function editHTML(element, parent_element) {
+    var current_text = $(element).html();
+    $(element).remove();
+    $(parent_element).append('<input type"text" id="rename-input" value=' + current_text + '/>');
+    $('input#rename-input').focusout(function() {
+        $(parent_element).append('<h1>' + $('input#rename-input').val() + '</h1>');
+        $('#rename-input').remove();
+    });
+}
 
 $(function() {
     $("div#rename-board form").on("submit", function(event) {
@@ -19,10 +28,11 @@ $(function() {
         var current_title = $("#menu-bar h1").html();
     });
 
-    $("a#board-title").on("click", function(event) {
-        var current_title = $("#menu-bar h1").html();
-        $("#rename-board-input").attr("value", current_title)
-        $("div#rename-board").toggle();
+    $("a#board-title h1").on("click", function(event) {
+        editHTML('#menu-bar h1', 'a#board-title');
+        // $('#board-title').append()
+        // $("#rename-board-input").attr("value", current_title)
+        // $("div#rename-board").toggle();
     });
 
     $("img#rename-board-close-button").on("click", function() {
