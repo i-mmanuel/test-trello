b7d340e6aab9a08933fa9b1ba78c76d718f1b500
Emmanuel <emmanuel.jnr@outlook.com>
2019-06-02 16:02:46 +0100

    Style Edit

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -18,6 +18,8 @@ function isEmpty(str) {
     return (!str || 0 === str.length);
 }
 
+// Click to edit function
+
 function editHTML(element, parent_element) {
     var current_text = $(element).html(),
         new_text;
@@ -26,36 +28,31 @@ function editHTML(element, parent_element) {
     $(parent_element).prepend('<input type="text" id="rename-input" onClick="this.select();" value=' + current_text + '/>');
     $(parent_element).removeClass('hover-highlight');
     
-    console.log('I see you!');
-
-    $('input#rename-input').focusout(function(event) {
-        // new_text = $('#rename-input').val();
-        // $(parent_element).append('<h1>' + new_text + '</h1>');
-        $('input#rename-input').remove();
-        // $('title').html(new_text + ' | Trello');
-    });
-
     $('input#rename-input').keypress(function(event) {
         if (event.which == 13) {
-            var new_text = $('input#rename-input').val();
-            $(parent_element).append('<h1>' + $('input#rename-input').val() + '</h1>');
+            var new_text = $('input#rename-input').html();
+            $(parent_element).append('<h1 class="board-name">' + $('input#rename-input').val() + '</h1>');
+            $(parent_element).addClass('hover-highlight');
             $('#rename-input').remove();
             $('title').html(new_text + ' | Trello');
         };
     });
+
+    $('input#rename-input').focusout(function(event) {
+        var new_text = $('input#rename-input').html();
+        $(parent_element).append('<h1 class="board-name hover-highlight">' + $('input#rename-input').val() + '</h1>');
+        $(parent_element).addClass('hover-highlight');
+        $('#rename-input').remove();
+        $('title').html(new_text + ' | Trello');
+    });
 };
 
 $(function() {
-    $("div#rename-board form").on("submit", function(event) {
-        event.preventDefault();
-        var current_title = $("#menu-bar h1").html();
-    });
-
     $('a#board-title').on('click', function(event) {
         event.preventDefault();
-    })
+    });
 
-    $("a#board-title h1").on("click", function(event) {
+    $("a#board-title h1.board-name").on("click", function(event) {
         event.preventDefault();
         editHTML('h1.board-name', 'a#board-title');
     });
